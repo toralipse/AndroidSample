2dd2d2e01faf65a32b1e5990e0d099916b84be57
toralipse <cruelty.tiger1226@gmail.com>
2016-06-20 19:17:27 +0900

    edittext add

--- app/src/main/java/com/example/toralipse/apptest/MainActivity.java
+++ app/src/main/java/com/example/toralipse/apptest/MainActivity.java
@@ -4,12 +4,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView label;
     private Button button;
+    private EditText editText;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -17,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         label = (TextView) findViewById(R.id.label);
         button = (Button)findViewById(R.id.button);
+        editText = (EditText)findViewById(R.id.editText);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
