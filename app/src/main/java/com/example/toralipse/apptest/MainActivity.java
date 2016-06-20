9949843c5aceb9f48864c78933063663b10112e5
toralipse <cruelty.tiger1226@gmail.com>
2016-06-20 19:53:43 +0900

    toast add

--- app/src/main/java/com/example/toralipse/apptest/MainActivity.java
+++ app/src/main/java/com/example/toralipse/apptest/MainActivity.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // action
+                Toast.makeText(MainActivity.this,editText.getEditableText().toString(),Toast.LENGTH_SHORT).show();
             }
         });
     }
