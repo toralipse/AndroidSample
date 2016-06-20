6bbaa188fc5efa6918c4b48fb5d4bbe76c21e952
toralipse <cruelty.tiger1226@gmail.com>
2016-06-20 18:37:04 +0900

    button add

--- app/src/main/java/com/example/toralipse/apptest/MainActivity.java
+++ app/src/main/java/com/example/toralipse/apptest/MainActivity.java
@@ -2,17 +2,26 @@ package com.example.toralipse.apptest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView label;
+    private Button button;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         label = (TextView) findViewById(R.id.label);
+        button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // action
+            }
+        });
     }
 }
