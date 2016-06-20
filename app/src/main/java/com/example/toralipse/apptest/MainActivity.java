f3bb2709f9d3f54c733ca584af1fab1fd676698f
toralipse <cruelty.tiger1226@gmail.com>
2016-06-20 20:04:42 +0900

    notify add

--- app/src/main/java/com/example/toralipse/apptest/MainActivity.java
+++ app/src/main/java/com/example/toralipse/apptest/MainActivity.java
@@ -1,13 +1,17 @@
 package com.example.toralipse.apptest;
 
+import android.support.v4.app.NotificationManagerCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.NotificationCompat;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final int NOTIFICATION_MINIMUM_ID = 0x1;
 
     private TextView label;
     private Button button;
@@ -23,7 +27,21 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // action
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(getApplicationContext());
+
+                String str = editText.getEditableText().toString();
+
+                builder.setSmallIcon(R.mipmap.ic_launcher);
+                builder.setContentTitle("1"+str);
+                builder.setContentText("2"+str);
+                builder.setSubText("3"+str);
+                builder.setContentInfo("4"+str);
+                builder.setWhen(System.currentTimeMillis());
+
+                builder.setTicker("5"+str);
+
+                NotificationManagerCompat manager = NotificationManagerCompat.from(getApplicationContext());
+                manager.notify(NOTIFICATION_MINIMUM_ID, builder.build());
             }
         });
     }
