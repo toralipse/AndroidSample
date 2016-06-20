07269baa637a404e23176b24ed25ab5a7dab6cdd
toralipse <cruelty.tiger1226@gmail.com>
2016-06-20 18:32:05 +0900

    toast implement

--- app/src/main/java/com/example/toralipse/apptest/MainActivity.java
+++ app/src/main/java/com/example/toralipse/apptest/MainActivity.java
@@ -14,5 +14,49 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         label = (TextView) findViewById(R.id.label);
+        label.setText("onCreate");
+        Toast.makeText(this, "onCreate",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        label.setText("onStart");
+        Toast.makeText(this, "onStart",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        label.setText("onResume");
+        Toast.makeText(this, "onResume",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        label.setText("onPause");
+        Toast.makeText(this, "onPause",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        label.setText("onStop");
+        Toast.makeText(this, "onStop",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        label.setText("onDestroy");
+        Toast.makeText(this, "onDestroy",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onRestart() {
+        super.onRestart();
+        label.setText("onRestart");
+        Toast.makeText(this, "onRestart",Toast.LENGTH_SHORT).show();
     }
 }
