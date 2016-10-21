010110d17e0c68bf0dba391171cdb39e7e781c23
toralipse <cruelty.tiger1226@gmail.com>
2016-10-21 17:14:53 +0900

    okhttpSample

--- app/src/main/java/com/example/toralipse/apptest/MainActivity.java
+++ app/src/main/java/com/example/toralipse/apptest/MainActivity.java
@@ -1,11 +1,16 @@
 package com.example.toralipse.apptest;
 
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,6 +29,42 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 // action
+                final String url = editText.getEditableText().toString();
+                if(url == null || url.equals("") ) return;
+                final Handler handler = new Handler();
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try{
+
+                            OkHttpClient client = new OkHttpClient();
+                            Request request = new Request.Builder()
+                                .url(url)
+                                .build();
+                            Response response = client.newCall(request).execute();
+                            final String message = response.body().string();
+                            handler.post(new Runnable() {
+                                @Override
+                                public void run() {
+                                    label.setText(message);
+                                }
+                            });
+
+                        }catch (Exception e) {
+
+                            e.printStackTrace();
+                            final String message = "Error";
+                            handler.post(new Runnable() {
+                                @Override
+                                public void run() {
+                                    label.setText(message);
+                                }
+                            });
+
+                        }
+
+                    }
+                }).start();
             }
         });
     }
